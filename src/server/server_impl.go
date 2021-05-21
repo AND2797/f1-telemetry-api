f64819fe2e77a1f369f7fb15264c63ad7ec7a04d
aditya <aditya.das2797@gmail.com>
2021-05-21 16:33:28 -0400

    testing concurrent data parsing

--- src/server/server_impl.go
+++ src/server/server_impl.go
@@ -13,6 +13,13 @@ import (
 func NewSession2018(HostPort string) *Session2018 {
 
 	s2018 := &Session2018{PacketHeader: udpHeaders.PacketHeader2018{}}
+	s2018.PacketCarSetupChannel = make(chan udpHeaders.PacketCarSetupData)
+	s2018.PacketCarStatusChannel = make(chan udpHeaders.PacketCarStatusData)
+	s2018.PacketCarTelemetryChannel = make(chan udpHeaders.PacketCarTelemetryData)
+	s2018.PacketEventChannel = make(chan udpHeaders.PacketEventData)
+	s2018.PacketLapChannel = make(chan udpHeaders.PacketLapData)
+	s2018.PacketMotionChannel = make(chan udpHeaders.PacketMotionData)
+
 	splitHostPort := strings.Split(HostPort, ":")
 	port, _ := strconv.Atoi(splitHostPort[1])
 	host := splitHostPort[0]
@@ -24,8 +31,16 @@ func NewSession2018(HostPort string) *Session2018 {
 	}
 	s2018.HostPort = HostPort
 	s2018.Server = ser
-
+	go s2018.getMotion()
 	return s2018
+}
+
+func (d *Data) getMotion() {
+
+	for {
+		d.PacketMotionData = <-d.PacketMotionChannel
+		fmt.Println(d.PacketMotionData)
+	}
 }
 
 func (s *Session2018) Listen() {
@@ -34,30 +49,17 @@ func (s *Session2018) Listen() {
 	for {
 		s.Server.ReadFromUDP(p)
 		headerReader := bytes.NewReader(p)
-		// motionReader := bytes.NewReader(p)
+		motionReader := bytes.NewReader(p)
 		// motionPacket := udpHeaders.PacketMotionData{}
 		s.PacketHeader = udpHeaders.PacketHeader2018{}
 		binary.Read(headerReader, binary.LittleEndian, &s.PacketHeader)
 		//send info via channel?
-		// switch packetId := s.PacketHeader.M_packetId; packetId{
-		// case udpHeaders.Motion:
-		// 	fmt.Println("motion")
-
-		// case udpHeaders.Session:
-
-		// case udpHeaders.Lap:
-
-		// case udpHeaders.Event:
-
-		// case udpHeaders.Participants:
-
-		// case udpHeaders.CarSetups:
-
-		// case udpHeaders.CarTelemetry:
-
-		// case udpHeaders.
-
-		// }
+		switch packetId := s.PacketHeader.M_packetId; packetId {
+		case udpHeaders.Motion:
+			motionPacket := udpHeaders.PacketMotionData{}
+			binary.Read(motionReader, binary.LittleEndian, &motionPacket)
+			s.PacketMotionChannel <- motionPacket
+		}
 	}
 }
 
--- src/server/server_structs.go
+++ src/server/server_structs.go
@@ -5,12 +5,8 @@ import (
 	"net"
 )
 
-type Session2018 struct {
-	HostPort string
-	Server   *net.UDPConn
-
-	PacketHeader           udpHeaders.PacketHeader2018
-	PacektMotionData       udpHeaders.PacketMotionData
+type Data struct {
+	PacketMotionData       udpHeaders.PacketMotionData
 	PacketSessionData      udpHeaders.PacketSessionData
 	PacketLapData          udpHeaders.PacketLapData
 	PacketEventData        udpHeaders.PacketEventData
@@ -18,10 +14,27 @@ type Session2018 struct {
 	PacketCarSetupData     udpHeaders.PacketCarSetupData
 	PacketCarTelemetryData udpHeaders.PacketCarTelemetryData
 	PacketCarStatusData    udpHeaders.PacketCarStatusData
+
+	PacketMotionChannel       chan udpHeaders.PacketMotionData
+	PacketSessionChannel      chan udpHeaders.PacketSessionData
+	PacketLapChannel          chan udpHeaders.PacketLapData
+	PacketEventChannel        chan udpHeaders.PacketEventData
+	PacketParticipantsChannel chan udpHeaders.PacketParticipantsData
+	PacketCarSetupChannel     chan udpHeaders.PacketCarSetupData
+	PacketCarTelemetryChannel chan udpHeaders.PacketCarTelemetryData
+	PacketCarStatusChannel    chan udpHeaders.PacketCarStatusData
+}
+
+type Session2018 struct {
+	HostPort     string
+	Server       *net.UDPConn
+	PacketHeader udpHeaders.PacketHeader2018
+	Data
 }
 
 type Session2020 struct {
 	HostPort     string
 	PacketHeader udpHeaders.PacketHeader2020
 	Server       *net.UDPConn
+	Data
 }
