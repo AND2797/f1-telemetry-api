1f4398faf0d86bca4b6697cab72cf47c249324e9
aditya <aditya.das2797@gmail.com>
2021-05-23 02:31:31 -0400

    pass pointer to channel

--- pkg/main.go
+++ pkg/main.go
@@ -13,9 +13,11 @@ func main() {
 	if args[0] == "2018" {
 		s2018 := server.NewSession2018("127.0.0.1:20777")
 		for msg := range s2018.DataChannel {
-			switch msg.(type) {
-			case data.CarTelemetryData:
-				fmt.Println("Telemetry!")
+			fmt.Printf("%T\n", msg)
+			switch m := msg.(type) {
+			case *data.PacketMotionData:
+				gforces := m.GetGForce(0)
+				fmt.Println(gforces)
 			}
 		}
 	}
