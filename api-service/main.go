f2d053f7a7b03a9870036d8ad093ed8c484b7800
AND2797 <aditya.das2797@gmail.com>
2021-10-07 01:19:09 -0400

    able to obtain telemetry data, bug in data order

--- api-service/main.go
+++ api-service/main.go
@@ -14,11 +14,11 @@ func main() {
 		ip := args[1]
 		s2018 := server.NewSession2018(ip)
 		for msg := range s2018.DataChans.DataChannel {
-			fmt.Println("Packets acct:", s2018.PacketsAcct)
-			switch msg.(type) {
-			case data.PacketMotionData2018:
-				fmt.Println("YO MOTION HERE")
-
+			switch packet := msg.(type) {
+			case data.PacketCarTelemetryData2018:
+				TelemetryData := packet.AllCarTelemetryData
+				Car1 := TelemetryData.CarList[0]
+				fmt.Println("Speed: ", Car1.Gear)
 			}
 
 		}
